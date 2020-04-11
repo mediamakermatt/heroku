5772d3ad35a4da61a5561f38d3d041723d427676
mediamakermatt <mediamakermatt@gmail.com>
2020-04-11 17:03:21 -0400

    toying around, testing for bugs

--- pages/home.php
+++ pages/home.php
@@ -1,44 +1,51 @@
-<!-- Full width column -->
-<div class="flex mb-4">
-  <div class="w-full bg-gray-500 h-12"></div>
+<div class="flex flex-row">
+    <div class="flex-none bg-gray-900 h-screen p-4 w-64">
+        <div class="flex flex-col">
+            <div class="w-full">
+                <img class="logo" src="../assets/images/svg/logo.svg"></img>
+            </div>
+            <ul class="list-none text-white pt-4">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+            <ul class="list-none text-white pt-4">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+            <ul class="list-none text-white pt-4">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="flex-grow bg-gray-600 h-screen p-4">
+        <div class="flex flex-col justify-center w-full bg-gray-300">
+            Search
+        </div>
+    </div>
 </div>
 
-<!-- Two columns -->
-<div class="flex mb-4">
-  <div class="w-1/2 bg-gray-400 h-12"></div>
-  <div class="w-1/2 bg-gray-500 h-12"></div>
-</div>
 
-<!-- Three columns -->
-<div class="flex mb-4">
-  <div class="w-1/3 bg-gray-400 h-12"></div>
-  <div class="w-1/3 bg-gray-500 h-12"></div>
-  <div class="w-1/3 bg-gray-400 h-12"></div>
-</div>
-
-<!-- Four columns -->
-<div class="flex mb-4">
-  <div class="w-1/4 bg-gray-500 h-12"></div>
-  <div class="w-1/4 bg-gray-400 h-12"></div>
-  <div class="w-1/4 bg-gray-500 h-12"></div>
-  <div class="w-1/4 bg-gray-400 h-12"></div>
-</div>
-
-<!-- Five columns -->
-<div class="flex mb-4">
-  <div class="w-1/5 bg-gray-500 h-12"></div>
-  <div class="w-1/5 bg-gray-400 h-12"></div>
-  <div class="w-1/5 bg-gray-500 h-12"></div>
-  <div class="w-1/5 bg-gray-400 h-12"></div>
-  <div class="w-1/5 bg-gray-500 h-12"></div>
-</div>
-
-<!-- Six columns -->
-<div class="flex">
-  <div class="w-1/6 bg-gray-400 h-12"></div>
-  <div class="w-1/6 bg-gray-500 h-12"></div>
-  <div class="w-1/6 bg-gray-400 h-12"></div>
-  <div class="w-1/6 bg-gray-500 h-12"></div>
-  <div class="w-1/6 bg-gray-400 h-12"></div>
-  <div class="w-1/6 bg-gray-500 h-12"></div>
-</div>
+<!-- <div class="w-full bg-gray-900 h-screen flex flex-col justify-between">
+    <div class="w-full bg-gray-600 h-10 flex flex-col justify-center text-white">
+        <ul class="list-none  pl-4">
+            <li class="inline pr-4"><a href="#">Link 1</a></li>
+            <li class="inline pr-4"><a href="#">Link 2</a></li>
+            <li class="inline pr-4"><a href="#">Link 3</a></li>
+            <li class="inline pr-4"><a href="#">Link 4</a></li>
+            <li class="inline pr-4"><a href="#">Link 5</a></li>
+        </ul>
+    </div>
+    <div class="text-center text-white text-6xl">
+        Content
+    </div>
+    <div class="w-full bg-gray-600 h-32 flex flex-col justify-center text-white">
+        <p class="text-center">Footer</p>
+    </div>
+</div> -->
